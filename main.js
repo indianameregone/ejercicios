24669ddb62e5263d7155fb208e6afd7285eaeab2
Juan Meregone <juanmeregone@gmail.com>
2022-01-21 23:45:44 -0300

    second commit

---
+++ main.js
@@ -0,0 +1,21 @@
+/*
+Crea una funcion que cuente la cantidad de caracteres de una cadena, ejemplo "hola mundo", 10 caracteres (el espacio cuenta)
+*/
+function contar (cadena){
+
+}
+
+/*
+Escribe una funcion que compare 3 numeros y diga cual es el mas grande, el mas pequeño o si son iguales, ejemplo
+2,3,4 = 4 El mas grande, 2 el mas pequeño
+*/
+function comparar (a,b,c){
+
+}
+
+/*Escribe una funcion que devuelva una cadena la cantidad de veces que fue pasado en el segundo argumento, Ejemplo
+let palabra = "hola mundo" function devolver(palabra, 3) devolvera ; "hola mundo", "hola mundo","hola mundo"
+*/
+function devolver(str){
+
+}
